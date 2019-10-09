86a670f055b1d17ca7c70a25b58cd50186794327
NexisSis <Nexis.sis@gmail.com>
2019-10-09 14:51:20 +0300

    refactoring dev folder

--- dev/index.jsx
+++ dev/index.jsx
@@ -1,13 +1,13 @@
 /* eslint-env browser */
 
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import Shiitake from '../src';
 
 import './index.css';
 
-const ipsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium tincidunt viverra. Pellentesque
+const ipsum1 = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium tincidunt viverra. Pellentesque
 auctor leo sit amet eros fringilla placerat. Proin et velit nec nulla laoreet sagittis. Nullam finibus lorem
 cursus, convallis diam nec, laoreet libero. In venenatis, risus sit amet lobortis commodo, dui nulla feugiat
 libero, sed pharetra enim mauris id turpis.`;
@@ -19,116 +19,99 @@ five centuries, but also the leap into electronic typesetting, remaining essenti
 popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently
 with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
 
-class App extends React.Component {
-  constructor() {
-    super();
+const App = () => {
+  const [ipsum, setIpsum] = useState(ipsum1);
+  const [dynamicLines, setDynamicLines] = useState(2);
+  const [truncatedText, setTruncatedText] = useState(false);
+  const text = 'Cook it up all night with Shitakes';
 
-    this.state = { ipsum, dynamicLines: 2, truncatedText: false };
+  // used to verify that passing undefined is safe
+  let nullText;
 
-    this.click = this.click.bind(this);
-    this.toggleDynamicLines = this.toggleDynamicLines.bind(this);
-    this.toggleTruncatedText = this.toggleTruncatedText.bind(this);
-  }
+  return (
+    <div className="wrapper">
+      <h1>Shiitake Demo</h1>
+      <p>Here are some examples of Shiitake in the wild.</p>
 
-  click() {
-    this.setState({ ipsum: ipsum2 });
-  }
-
-  toggleDynamicLines() {
-    this.setState({ dynamicLines: (this.state.dynamicLines === 2) ? 1 : 2 });
-  }
-
-  toggleTruncatedText(isTruncated) {
-    this.setState({ truncatedText: isTruncated });
-  }
-
-  render() {
-    const { truncatedText } = this.state;
-    const text = 'Cook it up all night with Shitakes';
-
-    // used to verify that passing undefined is safe
-    let nullText;
-
-    return (
-      <div className="wrapper">
-        <h1>Shiitake Demo</h1>
-        <p>
-          Here are some examples of Shiitake in the wild.
-        </p>
-
-        <h2>Compare with webkit line-clamp</h2>
-        <div className="side-by-side">
-          <Shiitake lines={2} throttleRate={200} className="shiitake">
-            {text}
-          </Shiitake>
-          <div className="line-clamp">
-            {text}
-          </div>
-        </div>
-
-        <h2>Create inline style with flexbox:</h2>
-        <div className="shiitake-inline-wrapper">
-          <Shiitake lines={1} throttleRate={200} className="shiitake-inline">
-            {text}
-          </Shiitake>
-          &gt;
-        </div>
-
-        <h2>Specify your own tag name:</h2>
-        <Shiitake tagName="p" lines={1} throttleRate={200} className="shiitake-paragraph" onClick={this.click}>
-          {this.state.ipsum}
+      <h2>Compare with webkit line-clamp</h2>
+      <div className="side-by-side">
+        <Shiitake lines={2} throttleRate={200} className="shiitake">
+          {text}
         </Shiitake>
+        <div className="line-clamp">
+          {text}
+        </div>
+      </div>
 
-        <h2>Can handle undefined children</h2>
-        <Shiitake lines={1}>
-          {nullText}
+      <h2>Create inline style with flexbox:</h2>
+      <div className="shiitake-inline-wrapper">
+        <Shiitake lines={1} throttleRate={200} className="shiitake-inline">
+          {text}
         </Shiitake>
+        &gt;
+      </div>
 
-        <h2>Example of showing/hiding</h2>
-        <div className="show-hide-example">
-          <h4>Hover here</h4>
-          <div className="show-hide-content">
-            <Shiitake lines={2}>
-              {ipsum}
-            </Shiitake>
-          </div>
-        </div>
+      <h2>Specify your own tag name:</h2>
+      <Shiitake tagName="p" lines={1} throttleRate={200} className="shiitake-paragraph" onClick={() => setIpsum(ipsum2)}>
+        {ipsum}
+      </Shiitake>
 
-        <h2>Change lines dynamically</h2>
-        <div>
-          <Shiitake lines={this.state.dynamicLines}>{ipsum}</Shiitake>
-          <button onClick={this.toggleDynamicLines}>Change to {(this.state.dynamicLines === 2) ? '1 line' : '2 lines'}</button>
-        </div>
+      <h2>Can handle undefined children</h2>
+      <Shiitake lines={1}>
+        {nullText}
+      </Shiitake>
 
-        <h2>Custom overflow node</h2>
-        <div>
-          <Shiitake
-            lines={2}
-            overflowNode={<a href="https://github.com/bsidelinger912/shiitake#readme" target="_blank" rel="noopener noreferrer"> ...read more</a>}
-          >
+      <h2>Example of showing/hiding</h2>
+      <div className="show-hide-example">
+        <h4>Hover here</h4>
+        <div className="show-hide-content">
+          <Shiitake lines={2}>
             {ipsum}
           </Shiitake>
         </div>
+      </div>
 
-        <h2>Detect if text is truncated</h2>
-        <div>
-          <Shiitake
-            lines={2}
-            onTruncationChange={this.toggleTruncatedText}
-          >
-            {ipsum}
-          </Shiitake>
-          <p className="example-label">
-            {truncatedText ? 'this text is truncated ^^' : 'this text is not truncated ^^'}
-          </p>
-        </div>
+      <h2>Change lines dynamically</h2>
+      <div>
+        <Shiitake lines={dynamicLines}>{ipsum}</Shiitake>
+        <button
+          onClick={() => setDynamicLines((dynamicLines === 2) ? 1 : 2)}
+        >
+          Change to {(dynamicLines === 2) ? '1 line' : '2 lines'}
+        </button>
+      </div>
 
-        <p className="read-more">
-          read more <a href="https://github.com/bsidelinger912/shiitake#readme">here</a>.
+      <h2>Custom overflow node</h2>
+      <div>
+        <Shiitake
+          lines={2}
+          overflowNode={
+            <a href="https://github.com/bsidelinger912/shiitake#readme" target="_blank" rel="noopener noreferrer">
+              ...read more
+            </a>
+          }
+        >
+          {ipsum}
+        </Shiitake>
+      </div>
+
+      <h2>Detect if text is truncated</h2>
+      <div>
+        <Shiitake
+          lines={2}
+          onTruncationChange={isTruncated => setTruncatedText(isTruncated)}
+        >
+          {ipsum}
+        </Shiitake>
+        <p className="example-label">
+          {truncatedText ? 'this text is truncated ^^' : 'this text is not truncated ^^'}
         </p>
       </div>
-    );
-  }
-}
+      <p className="read-more">
+        read more <a href="https://github.com/bsidelinger912/shiitake#readme">here</a>.
+      </p>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('react-root'));
--- dev/server.js
+++ dev/server.js
@@ -6,15 +6,11 @@ const React = require('react');
 const ReactDOMServer = require('react-dom/server'); // eslint-disable-line
 const Shiitake = require('../src').default;
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Shiitake lines={2} tagName="p">Hello</Shiitake>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Shiitake lines={2} tagName="p">Hello</Shiitake>
+  </div>
+);
 
 const string = ReactDOMServer.renderToString(<App />);
 
