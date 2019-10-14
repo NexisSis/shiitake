5bccbcd7be19726ed12a0e1dfc87c853a2dd05fb
NexisSis <Nexis.sis@gmail.com>
2019-10-14 12:00:38 +0300

    Refactor Resize Listener

--- src/ResizeListener.jsx
+++ src/ResizeListener.jsx
@@ -1,21 +1,41 @@
 import { useEffect } from 'react';
 import ResizeObserver from 'resize-observer-polyfill';
+import PropTypes from 'prop-types';
 
-export default (props) => {
+let observerStarted = true;
+const ResizeListener = (props) => {
   useEffect(() => {
     const element = props.resizeListenerRef.current;
+    // skip first render
     const resizeObserver = new ResizeObserver((entries) => {
-      if (!Array.isArray(entries) || !entries.length) {
+      if (observerStarted) {
+        observerStarted = false;
         return;
       }
 
+      if ((!Array.isArray(entries) || !entries.length)) {
+        return;
+      }
       props.handleResize();
     });
 
     resizeObserver.observe(element);
 
-    return () => resizeObserver.unobserve(element);
+    return () => {
+      resizeObserver.unobserve(element);
+      observerStarted = true;
+    };
   });
 
   return null;
 };
+
+ResizeListener.propTypes = {
+  handleResize: PropTypes.func.isRequired,
+  resizeListenerRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.any })
+  ]),
+};
+
+export default ResizeListener;
